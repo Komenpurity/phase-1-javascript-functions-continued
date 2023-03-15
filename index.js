e16e10d7d7d4032e1e2f7316061f6ca11ad2655c
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-15 13:02:31 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    let result = console.log(`This Saturday, I want to ${activity}!`);
+    return result;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork(activity="go to the office"){
+    let result = console.log(`This Monday, I will ${activity}`);
+    return result;
+}
+mondayWork();
+mondayWork("work from home");
+
+
+
+function wrapAdjective(){
+    return function (activity="special"){
+        let firstAdj = "*"
+        console.log(`You are ${firstAdj}a hard worker${firstAdj}!`);
+    }
+}
